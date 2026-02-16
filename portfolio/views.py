17326609b8f37d509f3df74f16d880ef9c0dff3c
Lucas Ferreira Garcez <lucasfgarcez@hotmail.com>
2026-02-16 19:26:34 -0300

    Primeiro commit - exercício Django

---
+++ portfolio/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+
+def contact(request):
+    return render(request, 'contact.html')
